ee1fe1e1b875140aa7e0d85833be8abb29055c16
tfoote <tfoote@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2010-06-17 22:26:11 +0000

    fix for stricter time

--- tf/src/static_transform_publisher.cpp
+++ tf/src/static_transform_publisher.cpp
@@ -76,7 +76,7 @@ int main(int argc, char ** argv)
 
     TransformSender tf_sender(atof(argv[1]), atof(argv[2]), atof(argv[3]),
                               atof(argv[4]), atof(argv[5]), atof(argv[6]), atof(argv[7]),
-                              ros::Time::now() + sleeper, //Future dating to allow slower sending w/o timeout
+                              ros::Time() + sleeper, //Future dating to allow slower sending w/o timeout
                               argv[8], argv[9]);
 
 
