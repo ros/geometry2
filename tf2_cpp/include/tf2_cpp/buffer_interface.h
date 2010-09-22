c7acb6feb8c14b7980698fcb419164dbcd0df1b8
Wim Meeussen <wim@willowgarage.com>
2010-09-22 14:31:13 -0700

    fix pure virtual functions

--- tf2_cpp/include/tf2_cpp/buffer_interface.h
+++ tf2_cpp/include/tf2_cpp/buffer_interface.h
@@ -89,13 +89,13 @@ public:
   // can transform with timeout, simple api
   virtual bool
     canTransform(const std::string& target_frame, const std::string& source_frame, 
-		 const ros::Time& target_time, const ros::Duration timeout) const = 0;
+		 const ros::Time& target_time, const ros::Duration timeout, std::string* errstr = NULL) const = 0;
 
   // can transform with timeout, advanced api
   virtual bool
     canTransform(const std::string& target_frame, const ros::Time& target_time,
 		 const std::string& source_frame, const ros::Time& source_time,
-		 const std::string& fixed_frame, const ros::Duration timeout) const = 0;
+		 const std::string& fixed_frame, const ros::Duration timeout, std::string* errstr = NULL) const = 0;
 
 
 
