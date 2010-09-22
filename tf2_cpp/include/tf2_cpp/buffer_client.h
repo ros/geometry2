2e6d32bf5ef9319f9183a23bdbe2ea633ed47383
Eitan Marder-Eppstein <eitan@willowgarage.com>
2010-09-22 12:41:57 -0700

    Adding some files for the buffer client

---
+++ tf2_cpp/include/tf2_cpp/buffer_client.h
@@ -0,0 +1,63 @@
+/*********************************************************************
+*
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2009, Willow Garage, Inc.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of Willow Garage, Inc. nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*
+* Author: Eitan Marder-Eppstein
+*********************************************************************/
+#ifndef TF2_CPP_BUFFER_CLIENT_H_
+#define TF2_CPP_BUFFER_CLIENT_H_
+
+#include <tf2_cpp/buffer_interface.h>
+#include <actionlib/client/simple_action_client.h>
+
+namespace tf2
+{
+  class BufferClient : public BufferInterface
+  {
+    public:
+      typedef actionlib::SimpleActionClient<tf2_msgs::LookupTransform> LookupActionClient;
+
+      virtual geometry_msgs::TransformStamped
+        lookupTransform(const std::string& target_frame, const std::string& source_frame,
+            const ros::Time& time, const ros::Duration timeout = ros::Duration(0.0)) const;
+
+      virtual geometry_msgs::TransformStamped 
+        lookupTransform(const std::string& target_frame, const ros::Time& target_time,
+            const std::string& source_frame, const ros::Time& source_time,
+            const std::string& fixed_frame, const ros::Duration timeout = ros::Duration(0.0)) const;
+
+    private:
+      LookupActionClient client_;
+  };
+};
+#endif
