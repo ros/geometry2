d0aa84947eb3a63f37cc0126adb219addf6c0f6a
Tully Foote <tfoote@willowgarage.com>
2010-09-28 14:58:28 -0700

    docs for cpp library porters

--- tf2_cpp/include/tf2_cpp/buffer_interface.h
+++ tf2_cpp/include/tf2_cpp/buffer_interface.h
@@ -40,27 +40,43 @@
 
 namespace tf2
 {
-    
-// this method needs to be implemented by client library developers
+  
+/**\brief The templated function expected to be able to do a transform
+ *
+ * This is the method which tf2 will use to try to apply a transform for any given datatype.   
+ * \param data_in The data to be transformed.
+ * \param data_out A reference to the output data.  Note this can point to data in and the method should be mutation safe.
+ * \param transform The transform to apply to data_in to fill data_out.  
+ * 
+ * This method needs to be implemented by client library developers
+ */
 template <class T>
-  void doTransform(const T& t_in, T& t_out, const geometry_msgs::TransformStamped& transform);
+  void doTransform(const T& data_in, T& data_out, const geometry_msgs::TransformStamped& transform);
 
-// method to extract timestamp from object
+/**\brief Get the timestamp from data 
+ * \param t The data input.
+ * \return The timestamp associated with the data. 
+ */
 template <class T>
   const ros::Time& getTimestamp(const T& t);
 
-// method to extract frame id from object
+/**\brief Get the frame_id from data 
+ * \param t The data input.
+ * \return The frame_id associated with the data. 
+ */
 template <class T>
   const std::string& getFrameId(const T& t);
 
-// method to extract timestamp from stamped object
+
+
+/* An implementation for Stamped<P> datatypes */
 template <class P>
   const ros::Time& getTimestamp(const tf2::Stamped<P>& t)
   {
     return t.stamp_;
   }
 
-// method to extract frame id from object
+/* An implementation for Stamped<P> datatypes */
 template <class P>
   const std::string& getFrameId(const tf2::Stamped<P>& t)
   {
