310f647451b517fe9d5c3f10d8fb0e2ab6a3eb6c
Tully Foote <tfoote@willowgarage.com>
2010-09-30 19:49:07 -0700

    adding import

--- tf2_py/src/tf2_py/__init__.py
+++ tf2_py/src/tf2_py/__init__.py
@@ -34,6 +34,7 @@
 #* 
 #* Author: Eitan Marder-Eppstein
 #***********************************************************
+from tf2 import *
 from buffer_interface import *
 from buffer import *
 from buffer_client import *
