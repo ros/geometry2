8ee25b81f57bcbbfb7e7638673a7bd13ba045e46
Laurent GEORGE <lgeorge@aldebaran-robotics.com>
2015-08-12 13:20:57 +0200

    Adding tests to package
    adding test to package.xml and CmakeList, also removing unused import

--- tf2_sensor_msgs/test/test_tf2_sensor_msgs.py
+++ tf2_sensor_msgs/test/test_tf2_sensor_msgs.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
-import roslib
 
-import sys
 import unittest
 import struct
 import tf2_sensor_msgs
@@ -9,7 +7,6 @@ from sensor_msgs import point_cloud2
 from sensor_msgs.msg import PointField
 from tf2_ros import TransformStamped
 import copy
-import rospy
 
 ## A sample python unit test
 class PointCloudConversions(unittest.TestCase):
@@ -38,7 +35,7 @@ class PointCloudConversions(unittest.TestCase):
         assert(list(point_cloud2.read_points(self.point_cloud_in)) == [(1.0, 2.0, 0.0), (10.0, 20.0, 30.0)])
 
     def test_simple_transform(self):
-        old_data = copy.deepcopy(self.point_cloud_in.data)  # deepcopy is not required here because we have a str
+        old_data = copy.deepcopy(self.point_cloud_in.data)  # deepcopy is not required here because we have a str for now
         point_cloud_transformed = tf2_sensor_msgs.do_transform_cloud(self.point_cloud_in, self.transform_translate_xyz_300)
 
         k = 300
@@ -47,3 +44,8 @@ class PointCloudConversions(unittest.TestCase):
         print("new_points are %s" % new_points)
         assert(expected_coordinates == new_points)
         assert(old_data == self.point_cloud_in.data)  # checking no modification in input cloud
+
+if __name__ == '__main__':
+    import rosunit
+    rosunit.unitrun("test_tf2_sensor_msgs", "test_point_cloud_conversion", PointCloudConversions)
+
