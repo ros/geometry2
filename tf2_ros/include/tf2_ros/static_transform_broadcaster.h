9eeb86f7614dbb443e1440d46a53dc95a9315a02
Karsten Knese <karsten.knese@googlemail.com>
2017-02-28 22:58:34 -0800

    additional changes

--- tf2_ros/include/tf2_ros/static_transform_broadcaster.h
+++ tf2_ros/include/tf2_ros/static_transform_broadcaster.h
@@ -52,6 +52,8 @@ public:
   /** \brief Constructor (needs a ros::Node reference) */
   StaticTransformBroadcaster();
 
+  StaticTransformBroadcaster(rclcpp::node::Node::SharedPtr node);
+
   /** \brief Send a TransformStamped message
    * The stamped data structure includes frame_id, and time, and parent_id already.  */
   void sendTransform(const geometry_msgs::msg::TransformStamped & transform);
