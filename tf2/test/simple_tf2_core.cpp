3234d918b98cd3c58e66c7e5240894f1ff05ce3e
Chris Lalancette <clalancette@osrfoundation.org>
2017-04-25 20:23:16 +0000

    Remove generate_rand_vectors() from a number of tests.
    It was never used, so there is no reason to carry it around.
    
    Signed-off-by: Chris Lalancette <clalancette@osrfoundation.org>

--- tf2/test/simple_tf2_core.cpp
+++ tf2/test/simple_tf2_core.cpp
@@ -34,27 +34,6 @@
 #include "tf2/LinearMath/Vector3.h"
 #include "tf2/exceptions.h"
 
-void seed_rand()
-{
-  //Seed random number generator with current microseond count
-  timeval temp_time_struct;
-  gettimeofday(&temp_time_struct,NULL);
-  srand(temp_time_struct.tv_usec);
-};
-
-void generate_rand_vectors(double scale, uint64_t runs, std::vector<double>& xvalues, std::vector<double>& yvalues, std::vector<double>&zvalues)
-{
-  seed_rand();
-  for ( uint64_t i = 0; i < runs ; i++ )
-  {
-    xvalues[i] = 1.0 * ((double) rand() - (double)RAND_MAX /2.0) /(double)RAND_MAX;
-    yvalues[i] = 1.0 * ((double) rand() - (double)RAND_MAX /2.0) /(double)RAND_MAX;
-    zvalues[i] = 1.0 * ((double) rand() - (double)RAND_MAX /2.0) /(double)RAND_MAX;
-  }
-}
-
-
-
 TEST(tf2, setTransformFail)
 {
   tf2::BufferCore tfc;
--- tf2_ros/test/listener_unittest.cpp
+++ tf2_ros/test/listener_unittest.cpp
@@ -31,27 +31,6 @@
 #include <tf2_ros/transform_listener.h>
 #include <sys/time.h>
 
-
-void seed_rand()
-{
-  //Seed random number generator with current microseond count
-  timeval temp_time_struct;
-  gettimeofday(&temp_time_struct,NULL);
-  srand(temp_time_struct.tv_usec);
-};
-
-void generate_rand_vectors(double scale, uint64_t runs, std::vector<double>& xvalues, std::vector<double>& yvalues, std::vector<double>&zvalues)
-{
-  seed_rand();
-  for ( uint64_t i = 0; i < runs ; i++ )
-  {
-    xvalues[i] = 1.0 * ((double) rand() - (double)RAND_MAX /2.0) /(double)RAND_MAX;
-    yvalues[i] = 1.0 * ((double) rand() - (double)RAND_MAX /2.0) /(double)RAND_MAX;
-    zvalues[i] = 1.0 * ((double) rand() - (double)RAND_MAX /2.0) /(double)RAND_MAX;
-  }
-}
-
-
 using namespace tf2;
 
 TEST(tf2_ros_transform, transform_listener)
