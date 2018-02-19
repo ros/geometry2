9d16fdd57e8802527b5c482623e32f62b93d4852
Christopher Wecht <christopher.wecht@student.kit.edu>
2018-02-19 17:43:36 +0100

    tf2_ros::Buffer: both version of cantransform can now deal with smaller timeouts

--- tf2_ros/src/buffer.cpp
+++ tf2_ros/src/buffer.cpp
@@ -158,12 +158,13 @@ Buffer::canTransform(const std::string& target_frame, const ros::Time& target_ti
 
   // poll for transform if timeout is set
   ros::Time start_time = now_fallback_to_wall();
+  const ros::Duration sleep_duration = timeout * 0.001;
   while (now_fallback_to_wall() < start_time + timeout && 
          !canTransform(target_frame, target_time, source_frame, source_time, fixed_frame) &&
          (now_fallback_to_wall()+ros::Duration(3.0) >= start_time) &&  //don't wait when we detect a bag loop
          (ros::ok() || !ros::isInitialized())) // Make sure we haven't been stopped (won't work for pytf)
          {  
-           sleep_fallback_to_wall(ros::Duration(0.01));
+           sleep_fallback_to_wall(sleep_duration);
          }
   bool retval = canTransform(target_frame, target_time, source_frame, source_time, fixed_frame, errstr);
   conditionally_append_timeout_info(errstr, start_time, timeout);
