ros/geometry2

commit 6c5ac09d8a2f046e16100d49543c3e345b1dd20d

remove extra erase()

tf2/src/buffer_core.cpp
@@ -1259,8 +1259,7 @@ void BufferCore::removeTransformableCallback(TransformableCallbackHandle handle)
 
   {
     boost::mutex::scoped_lock lock(transformable_requests_mutex_);
-    V_TransformableRequest::iterator it = std::remove_if(transformable_requests_.begin(), transformable_requests_.end(), RemoveRequestByCallback(handle));
-    transformable_requests_.erase(it, transformable_requests_.end());
+    std::remove_if(transformable_requests_.begin(), transformable_requests_.end(), RemoveRequestByCallback(handle));
   }
 }
 
@@ -1336,12 +1335,7 @@ struct BufferCore::RemoveRequestByID
 void BufferCore::cancelTransformableRequest(TransformableRequestHandle handle)
 {
   boost::mutex::scoped_lock lock(transformable_requests_mutex_);
-  V_TransformableRequest::iterator it = std::remove_if(transformable_requests_.begin(), transformable_requests_.end(), RemoveRequestByID(handle));
-
-  if (it != transformable_requests_.end())
-  {
-    transformable_requests_.erase(it, transformable_requests_.end());
-  }
+  std::remove_if(transformable_requests_.begin(), transformable_requests_.end(), RemoveRequestByID(handle));
 }
 
 

commit b4ede6dcb4c60c82a6754f978a4fe45a93148db2

Fix dead loop in message filter
The bug is introduced in https://github.com/ros/geometry2/pull/461

`std::remove_if()` will only put elements to the back of the vector. One must call erase to actually remove them.

tf2/src/buffer_core.cpp
@@ -1337,7 +1337,11 @@ struct BufferCore::RemoveRequestByID
 void BufferCore::cancelTransformableRequest(TransformableRequestHandle handle)
 {
   boost::mutex::scoped_lock lock(transformable_requests_mutex_);
-  std::remove_if(transformable_requests_.begin(), transformable_requests_.end(), RemoveRequestByID(handle));
+  auto it = std::remove_if(transformable_requests_.begin(), transformable_requests_.end(), RemoveRequestByID(handle));
+  if (it != transformable_requests_.end())
+  {
+    transformable_requests_.erase(it, transformable_requests_.end());
+  }
 }
 
 